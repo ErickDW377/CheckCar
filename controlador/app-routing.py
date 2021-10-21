a72d433301c0312a17bfe20846ed394bef8ed662
Erick Diaz Wences <erickcuate1@gmail.com>
2021-10-20 21:57:22 -0500

    inicio

--- controlador/app-routing.py
+++ controlador/app-routing.py
@@ -6,6 +6,21 @@ app=Flask(__name__,template_folder='../pages',static_folder='../static')
 def incio():    
     return  render_template('inicio/inicio.html')
 
+@app.route('/login')
+def login():    
+    return  render_template('login/login.html')
+
+@app.route('/servicios')
+def servicios():    
+    return  render_template('servicios/servicios.html')  
+
+@app.route('/registrarServicio')
+def registrarServicio():    
+    return  render_template('servicios/servicios-registrar.html') 
+
+@app.route('/editarServicio')
+def editarServicio():    
+    return  render_template('servicios/servicios-editar.html')    
 
 if __name__=='__main__':
     app.run(debug=True)
